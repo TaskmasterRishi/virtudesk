18ed2d2b93c33ada1ef0c22c0e8604c6371a77ab
Rishi Patodiya <rishipatodiya12@gmail.com>
2025-07-28 12:27:09 +0530

    signin and signup page update

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,21 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-purple-50">
-      <SignIn />
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-50 to-purple-50">
+        <SignIn
+          appearance={{
+            elements: {
+              formButtonPrimary: {
+                fontSize: "14px",
+                textTransform: "none",
+                backgroundColor: "#611BBD",
+                "&:hover, &:focus, &:active": {
+                  backgroundColor: "#49247A",
+                },
+              },
+            },
+          }}
+        />
     </div>
   );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,20 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-purple-50">
-      <SignUp />
+      <SignUp 
+         appearance={{
+          elements: {
+            formButtonPrimary: {
+              fontSize: "14px",
+              textTransform: "none",
+              backgroundColor: "#611BBD",
+              "&:hover, &:focus, &:active": {
+                backgroundColor: "#49247A",
+              },
+            },
+          },
+        }}
+      />
     </div>
   );
 }
