0eca3ea44238f89a01e4620183fc5921dc009230
Rishi Patodiya <rishipatodiya12@gmail.com>
2025-08-21 20:08:36 +0530

    Multiplayer added

--- app/room/[roomId]/page.tsx
+++ app/room/[roomId]/page.tsx
@@ -3,11 +3,12 @@ import { Suspense } from 'react'
 import PhaserMap from '@/game/PhaserMap'
 import {LoaderTwo } from '@/components/ui/loader'
 
-export default function RoomPage({ params }: { params: { roomId: string } }) {
+export default async function RoomPage({ params }: { params: Promise<{ roomId: string }> }) {
+  const { roomId } = await params
   return (
     <div className="w-full h-screen">
       <Suspense fallback={<div className="w-full h-screen flex items-center justify-center"><LoaderTwo/></div>}>
-        <PhaserMap roomId={params.roomId} />
+        <PhaserMap roomId={roomId} />
       </Suspense>
     </div>
   )
